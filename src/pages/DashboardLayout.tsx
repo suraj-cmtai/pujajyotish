9cd43a40bcf57fb0c25ba021bee6b9baca6ceccf
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-07-11 14:53:25 +0530

    fix

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -32,7 +32,7 @@ const DashboardLayout = () => {
       )}
       <div className="flex-1 flex flex-col">
         <Header title="Dashboard" onMenuClick={() => setSidebarOpen(true)} />
-        <main className="flex-1 p-2 flex justify-center items-center">
+        <main className="flex-1 p-2 flex justify-center items-start">
           <Outlet />
         </main>
       </div>
